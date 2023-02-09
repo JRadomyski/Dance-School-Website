f28854121cfd7ad713955b2676eb66b22c29bc80
jaayrad <zakubrad@gmail.com>
2023-02-09 19:52:21 +0100

    FINAL

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Dance School Template
+Project start: 08.2022.
+Project release: 10.2022.
+
+
+Samples:
+
+![Screenshot](index.png)
+![Screenshot](aboutus.png)
+![Screenshot](pricing.png)
+![Screenshot](contact.png)
+
+URL: www.taniecsiedlce.com
--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const h1Text = text.textContent;
 const split_h1 = h1Text.split("");
 text.textContent="";
 
-
 for(let v=0; v < split_h1.length; v++){
     text.innerHTML += "<span>" + split_h1[v] + "</span>";
 }
-
 
 let char = 0;
 let timer = setInterval(onTick, 90);
@@ -18,7 +16,6 @@ function complete() {
     clearInterval(timer);
     timer = null;
 }
-
 
 function onTick(){
     const span = text.querySelectorAll('span')[char];
