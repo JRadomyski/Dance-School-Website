cc8312e1ce7e4a8ff159c28a1f7bcb653abc1328
Jakub Radomyski <zakubrad@gmail.com>
2023-08-06 13:06:41 +0200

    Banner-text disable

--- script.js
+++ script.js
@@ -15,34 +15,34 @@ menuBtn.onclick = function(){
     }
 }
 
-//banner-text animation
-const text = document.querySelector(".awesome_p");
-const h1Text = text.textContent;
-const split_h1 = h1Text.split("");
-text.textContent="";
-
-for(let v=0; v < split_h1.length; v++){
-    text.innerHTML += "<span>" + split_h1[v] + "</span>";
-}
-
-let char = 0;
-let timer = setInterval(onTick, 90);
-
-
-function complete() {
-    clearInterval(timer);
-    timer = null;
-}
-
-function onTick(){
-    const span = text.querySelectorAll('span')[char];
-    span.classList.add('fade');
-    char++ 
-    if(char === split_h1.length){
-        complete();
-        return
-    }
-}
+// //banner-text animation
+// const text = document.querySelector(".awesome_p");
+// const h1Text = text.textContent;
+// const split_h1 = h1Text.split("");
+// text.textContent="";
+//
+// for(let v=0; v < split_h1.length; v++){
+//     text.innerHTML += "<span>" + split_h1[v] + "</span>";
+// }
+//
+// let char = 0;
+// let timer = setInterval(onTick, 90);
+//
+//
+// function complete() {
+//     clearInterval(timer);
+//     timer = null;
+// }
+//
+// function onTick(){
+//     const span = text.querySelectorAll('span')[char];
+//     span.classList.add('fade');
+//     char++
+//     if(char === split_h1.length){
+//         complete();
+//         return
+//     }
+// }
 
 // Year-Holding
 const yearInFooter = document.querySelector('.footer__year');
