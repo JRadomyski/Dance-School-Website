1783dd843d8a0459bb9bd43baaa2ef530ce0a175
Jakub Radomyski <zakubrad@gmail.com>
2023-10-28 19:06:51 +0200

    Handling menubtn better

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
+const yearInFooter = document.querySelector(".footer__year");
 const menuBtn = document.getElementById("menubtn");
 const sideNav = document.getElementById("sideNav");
 const menu = document.getElementById("menu");
+
 sideNav.style.right = "-250px";
 
+menuBtn.onclick = function () {
+  if (sideNav.style.right === "-250px") {
+    sideNav.style.right = "0";
+    menu.src = "images/x.svg";
+  } else {
+    sideNav.style.right = "-250px";
+    menu.src = "images/menu.svg";
+  }
+};
 
-menuBtn.onclick = function(){
-    if(sideNav.style.right == "-250px"){
-        sideNav.style.right = "0";
-        menu.src="images/x.svg";
-    }
-    else{
-        sideNav.style.right="-250px";
-        menu.src="images/menu.svg"
-    }
+function clickOutside(event) {
+  const isClickInside = sideNav.contains(event.target);
+  const isMenuButtonClicked = menuBtn.contains(event.target);
+
+  if (!isClickInside && !isMenuButtonClicked) {
+    sideNav.style.right = "-250px";
+    menu.src = "images/menu.svg";
+  }
 }
 
-const yearInFooter = document.querySelector('.footer__year');
+document.addEventListener("click", clickOutside);
+
 const CurrentYear = () => {
-    const year = (new Date).getFullYear();
-    yearInFooter.innerText = year;
-}
+  yearInFooter.innerText = new Date().getFullYear();
+};
 
 CurrentYear();
